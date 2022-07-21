7bff58260ac82519903fc43d420caacba77970d3
pdsuwwz <pdsu.wwz@foxmail.com>
2022-07-21 11:16:11 +0800

    ⭐️ feat: upgrade webpack

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import { createApp } from 'vue'
 import router from '@/router'
-import CustomPackage from '@/main'
 import App from '@/App.vue'
 
 import '@/styles/index.scss'
 
 createApp(App)
   .use(router)
-  .use(CustomPackage)
   .mount('#app')
